b101eb594a1b74c1a8f435b8c3671bd737731f52
Zach <Zach@Zanzibars-Computer.local>
2023-03-27 20:46:05 -0700

    css

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jenkemmag.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          not helpful link
         </a>
       </header>
     </div>
